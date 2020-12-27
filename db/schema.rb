5df27e9b856b9c7974bb89fbdcced7d4ee1c8f42
Kulebyakin Andrey <ankulebyakin@gmail.com>
2020-12-27 23:44:37 +0800

    Create migration seeding table seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_144951) do
+ActiveRecord::Schema.define(version: 2020_12_27_233000) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "seeds", force: :cascade do |t|
     t.string "title"

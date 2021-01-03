75fe9a9394c4f75d2a600385db640662c4fcbf98
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-04 02:10:09 +0800

    Add authenticate_admin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def authenticate_admin!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "Unauthorized Access"
+      redirect_to root_path
+    end
+  end
 end

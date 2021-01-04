96d6d666986834d80178ac2400c3c39cc7da2798
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-04 13:49:17 +0800

    Add to dashboard 10 recent users and seeds

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,43 +2,26 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    # panel "Seeds" do
-    #   Seed.all().map do |seed|
-    #     columns do
-    #       column do
-    #         span seed.title 
-    #       end
-    #       column do
-    #         span seed.latin_title 
-    #       end
-    #     end
-    #   end
-    # end
-    # div class: "blank_slate_container", id: "dashboard_default_message" do
-    #   span class: "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-      # div do
-        # panel "Info" do
-        #   para "Welcome to ActiveAdmin."
-        # end
-      # end
-    # end
+    columns do
+      panel "Info" do
+        para "Welcome to ActiveAdmin."
+      end
+      column do
+        panel "Recent Users" do
+          ul do
+            User.last(10).reverse.map do |user|
+              li link_to(user.username, admin_user_path(user))
+            end
+          end
+        end
+        panel "Recent Seeds" do
+          ul do
+            Seed.last(10).reverse.map do |seed|
+              li link_to(seed.title, admin_seed_path(seed))
+            end
+          end
+        end
+      end
+    end
   end # content
 end

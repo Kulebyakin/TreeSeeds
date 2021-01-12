dccefcf1ffe5311dbf3f81063806af8e041382ea
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-12 16:58:31 +0800

    Do not open cart page if cart is empty

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,12 @@
 class CartController < ApplicationController
   def index
     @order_input = params[:orders]
+
+    if @order_input.empty?
+      flash[:alert] = "Ваша корзина пуста. Добавьте желаемые товары в корзину."
+      redirect_to root_path
+    end 
+    
     @items = parse_orders(@order_input)
     
     @items.each do |item|

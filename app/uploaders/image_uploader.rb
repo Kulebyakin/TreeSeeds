393bb4e2b679b0a9b920dcf686f0a598abe613b6
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-13 22:17:38 +0800

    Add size of images thumb100

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -37,6 +37,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process resize_to_fill: [200, 200]
   end
 
+  version :thumb100 do
+    process resize_to_fit: [100, nil]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist

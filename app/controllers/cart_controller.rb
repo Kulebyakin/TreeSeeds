2d6674542c5ca89b2cb5c93c41e6d9f268b2484b
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-15 11:16:35 +0800

    Refactoring after adding JSON

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,13 +5,14 @@ class CartController < ApplicationController
     if @order_input.empty?
       flash[:alert] = "Ваша корзина пуста. Добавьте желаемые товары в корзину."
       redirect_to root_path
-    end 
-
-    @items = JSON.parse(@order_input)
-    
-    @items.each do |item|
-      item["id"] = Seed.find(item["id"])
+    else
+      @items = JSON.parse(@order_input)
+      
+      @items.each do |item|
+        item["id"] = Seed.find(item["id"])
+      end
     end
+    
   end
 
 end

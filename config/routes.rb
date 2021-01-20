156d2b3c52b5694b314cb46254cfb83f05a4a0b0
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-20 22:01:43 +0800

    Add orders routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   post 'cart' => "cart#index"
 
   resources :seeds
+  resources :orders
   root to: 'seeds#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

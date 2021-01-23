c4b304f4e371774ba71e6920a178bae6d32e1203
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-23 20:09:32 +0800

    Add user_id to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_20_103238) do
+ActiveRecord::Schema.define(version: 2021_01_23_095641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2021_01_20_103238) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["region_id"], name: "index_orders_on_region_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "prices", force: :cascade do |t|

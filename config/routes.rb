26ac892a54293225de56f01a03a04f3775641e8b
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-01-30 17:38:39 +0800

    Add route to show order by digits

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,11 @@ Rails.application.routes.draw do
 
   get 'seeds/:id', to: 'seeds#show', constraints: { id: /\d+/ }
   get 'seeds/:topic', to: 'seeds#topic'
-  get 'orders/:finished', to: 'orders#finished'
-
 
   resources :seeds
+  
+  get 'orders/:id', to: 'orders#show', constraints: { id: /\d+/ }
+  get 'orders/:finished', to: 'orders#finished'
   resources :orders
   root to: 'seeds#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

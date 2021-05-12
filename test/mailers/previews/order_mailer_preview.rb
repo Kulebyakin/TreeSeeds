387d998ccee451ddc26b973138ef454be6377e62
Kulebyakin Andrey <ankulebyakin@gmail.com>
2021-05-12 21:49:26 +0800

    Load last order to mailer preview

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -2,7 +2,7 @@
 class OrderMailerPreview < ActionMailer::Preview
   def new_order_email
     # Set up a temporary order for the preview
-    order = Order.new(full_name: "Joe Smith", zip: 12, address: "1-2-3 Chuo, Tokyo, 333-0000", phone: "090-7777-8888", order: "I want to place an order!")
+    order = Order.last
 
     OrderMailer.with(order: order).new_order_email
   end
